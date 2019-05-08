68d45dcaf2f0bff27efb17986ef770e073de9e24
hao <hao@haos-MacBook-Pro.local>
2019-05-08 16:05:33 +0800

    Modified main function

--- requests.c
+++ requests.c
@@ -255,16 +255,22 @@ int main() {
     struct request s_request;
     bzero(&s_request, sizeof(struct request));
     s_request.method = "GET";
-    char *headers[3];
-    headers[0] = "Host: www.baidu.com";
-    headers[1] = "Connection: close";
-    headers[2] = NULL;
+    char *headers[2];
+    headers[0] = "Connection: close";
+    headers[1] = NULL;
     s_request.headers = headers;
     char response_buff[600000];
     requests("http://www.baidu.com/", &s_request, response_buff);
-    printf("response_buff is:\n%s\n", response_buff);
     struct response s_response;
     parse_response(response_buff, &s_response);
+    
+    printf("status_code: %d\n", s_response.status_code);
+    printf("\n");
+    printf("reason: %s\n", s_response.reason);
+    printf("\n");
+    printf("headers:\n");
     print_table(s_response.headers);
+    printf("\n");
+    printf("content:\n%s", s_response.content);
     return 0;
 }
